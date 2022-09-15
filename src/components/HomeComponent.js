fa5ea67fad78565f32083bb4a0763549557b4868
Takdirul Islam Shishir <39631235+iamtakdir@users.noreply.github.com>
2022-09-15 20:13:49 +0600

    redirect issue resolved

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,94 +3,94 @@ import React, { useState, useEffect } from "react";
 import getData from "../utils/shortLink";
 
 const HomeComponent = () => {
-	const [data, setData] = useState(null);
-	const currentUrl = window.location.href;
+  const [data, setData] = useState(null);
+  const currentUrl = window.location.href;
 
-	const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	const onFinish = async (values) => {
-		const resp = await getData(values);
-		setData(resp.data);
+  const onFinish = async (values) => {
+    const resp = await getData(values);
+    setData(resp.data);
 
-		setLoading(false);
-	};
+    setLoading(false);
+  };
 
-	const onFinishFailed = (errorInfo) => {
-		console.log("Failed:", errorInfo);
-		setLoading(false);
-	};
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    setLoading(false);
+  };
 
-	console.log(data);
+  console.log(data);
 
-	return (
-		<div key={data}>
-			<div style={{ marginTop: "70px" }}>
-				<div className='h-100 d-flex align-items-center justify-content-center'>
-					<div>
-						<div className='text-center mb-4 '>
-							<h1 className='display-4'>Welcome to Uink</h1>
-							<p>SHORT YOUR LINK EASILY </p>
-						</div>
+  return (
+    <div key={data}>
+      <div style={{ marginTop: "70px" }}>
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          <div>
+            <div className="text-center mb-4 ">
+              <h1 className="display-4">Welcome to Uink</h1>
+              <p>SHORT YOUR LINK EASILY </p>
+            </div>
 
-						<div className='card'>
-							<div className='card-body'>
-								<h5 className='card-title m-3'>Shorten your link</h5>
-								<Form
-									name='basic'
-									labelCol={{
-										span: 6,
-									}}
-									wrapperCol={{
-										span: 16,
-									}}
-									initialValues={{
-										remember: true,
-									}}
-									onFinish={onFinish}
-									onFinishFailed={onFinishFailed}
-									autoComplete='off'>
-									<Form.Item
-										label='URL'
-										name='url'
-										rules={[
-											{
-												required: true,
-												message: "Please input URL!",
-											},
-										]}>
-										<Input />
-									</Form.Item>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title m-3">Shorten your link</h5>
+                <Form
+                  name="basic"
+                  labelCol={{
+                    span: 6,
+                  }}
+                  wrapperCol={{
+                    span: 16,
+                  }}
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  autoComplete="off">
+                  <Form.Item
+                    label="Link"
+                    name="Link"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input URL!",
+                      },
+                    ]}>
+                    <Input />
+                  </Form.Item>
 
-									<Form.Item
-										wrapperCol={{
-											offset: 6,
-											span: 16,
-										}}>
-										<Button
-											onClick={() => setLoading(true)}
-											block
-											type='primary'
-											htmlType='submit'
-											loading={loading}>
-											Submit
-										</Button>
-									</Form.Item>
-								</Form>
-								{data && (
-									<h5 className='mt-4'>
-										{" "}
-										Your Link :{" "}
-										<a
-											href={`${currentUrl}${data}`}>{`${currentUrl}${data}`}</a>
-									</h5>
-								)}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                  <Form.Item
+                    wrapperCol={{
+                      offset: 6,
+                      span: 16,
+                    }}>
+                    <Button
+                      onClick={() => setLoading(true)}
+                      block
+                      type="primary"
+                      htmlType="submit"
+                      loading={loading}>
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Form>
+                {data && (
+                  <h5 className="mt-4">
+                    {" "}
+                    Your Link :{" "}
+                    <a
+                      href={`${currentUrl}${data}`}>{`${currentUrl}${data}`}</a>
+                  </h5>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeComponent;
--- src/components/redirect.js
+++ src/components/redirect.js
@@ -1,41 +1,64 @@
-import React, { useState } from "react";
-import { Navigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Button, Layout } from "antd";
 import axios from "axios";
 
+const getUrl = async (id) => {
+  const response = await axios({
+    method: "get",
+    url: `/${id}`,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  });
+
+  return response.data.url;
+};
+
 const Redirect = () => {
-	const [url, setUrl] = useState("");
-	const { id } = useParams();
+  const { Footer } = Layout;
+  const [url, setUrl] = useState("");
+  const { id } = useParams();
+  let newUrl = "";
 
-	try {
-		(async (id) => {
-			const response = await axios({
-				method: "get",
-				url: `/${id}`,
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json;charset=UTF-8",
-				},
-			});
+  useEffect(() => {
+    const resp = getUrl(id).then((data) => setUrl(data));
+  }, []);
 
-			setUrl(response.data.url);
-		})(id);
+  if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
+    // window.location.replace(`${url}`);
+    newUrl = `${url}`;
+  } else {
+    // window.location.replace(`https://${url}`);
+    newUrl = `https://${url}`;
+  }
 
-		if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
-			// window.location.replace(`${url}`);
-			setTimeout(() => {
-				return <Navigate to={url} replace='true' />;
-			}, 5000);
-		} else {
-			// window.location.replace(`https://${url}`);
-			setTimeout(() => {
-				<Navigate to={`https://${url}`} replace='true' />;
-			}, 5000);
-		}
-	} catch (error) {
-		console.log(error);
-	}
-
-	return;
+  return (
+    <div className="center" style={{ marginTop: "150px" }}>
+      {newUrl ? (
+        <div className="">
+          <h3 className="display-4"> Here is Your Link Click to go</h3>
+          <a href={`${newUrl}`}>
+            <Button size="large" type="primary" shape="round">
+              {" "}
+              Click Here{" "}
+            </Button>
+            <p className="lead m-3"> Your link is {newUrl}</p>
+          </a>
+        </div>
+      ) : (
+        <h2> Please wait </h2>
+      )}
+      <div className="fixed-bottom">
+        <Footer style={{ textAlign: "center" }}>
+          A Project of
+          <a href="https://iamtakdir.xyz"> Takdirul Islam Shishir</a> Uink- URL
+          Shortner.
+        </Footer>
+      </div>
+    </div>
+  );
 };
 
 export default Redirect;
